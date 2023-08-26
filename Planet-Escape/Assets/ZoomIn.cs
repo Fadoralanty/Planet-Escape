9ce3a2cf49befa8044c26f18b5a19a29541174e8
Julian Lanty <julianemanueltejo@gmail.com>
2023-08-26 02:28:28 -0300

    Highlight card on Hover
    Return card to hand if not played

--- Planet-Escape/Assets/ZoomIn.cs
+++ Planet-Escape/Assets/ZoomIn.cs
@@ -7,7 +7,10 @@ using UnityEngine.EventSystems;
 public class ZoomIn : MonoBehaviour, IPointerExitHandler, IPointerEnterHandler
 {
     [SerializeField] private Transform handTransform;
+    [SerializeField] private float onMouseHoverHeight = 150f;
     private Animator _animator;
+    private Quaternion _lastRotation;
+    private Vector2 _lastPosition;
     private int lastSiblingIndex;
     private void Awake()
     {
@@ -17,18 +20,25 @@ public class ZoomIn : MonoBehaviour, IPointerExitHandler, IPointerEnterHandler
 
     private void Start()
     {
-        lastSiblingIndex = transform.GetSiblingIndex();
-    }
-
-    public void OnPointerExit(PointerEventData eventData)
-    {
-        _animator.Play("OnHoverExit");
-        transform.SetSiblingIndex(lastSiblingIndex);
+        Transform transform1 = transform;
+        lastSiblingIndex = (transform1).GetSiblingIndex();
+        _lastRotation = transform1.rotation;
+        _lastPosition = transform1.position;
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         _animator.Play("OnHoverEnter");
         transform.SetSiblingIndex(handTransform.childCount);
+        transform.rotation = Quaternion.Euler(Vector3.zero);
+        transform.position = new Vector3(transform.position.x, onMouseHoverHeight);
     }
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        _animator.Play("OnHoverExit");
+        transform.SetSiblingIndex(lastSiblingIndex);
+        transform.rotation = _lastRotation;
+        transform.position = _lastPosition;
+    }
+
 }
