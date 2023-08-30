f272308a2c41133d61a12c0bf218a4e3104f90d6
Julian Lanty <julianemanueltejo@gmail.com>
2023-08-30 03:03:50 -0300

    Player and health bar

---
+++ Planet-Escape/Assets/Cards/Scripts/Damageable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class Damageable : MonoBehaviour
+{
+    public Action<float> OnTakeDamage { get; set; }
+    public Action OnDie { get; set; }
+    public float MaxLife => maxLife;
+    [SerializeField] private float maxLife = 100;
+    [SerializeField] private float _currentLife;
+
+    private void Awake()
+    {
+        SetCurrentLife(maxLife);
+    }
+
+    public bool IsAlive() => _currentLife > 0;
+    public void SetCurrentLife(float life) => _currentLife = life;
+    public void TakeDamage(float damage)
+    {
+        if (IsAlive())
+        {
+            _currentLife -= damage;
+            OnTakeDamage?.Invoke(_currentLife);
+        }
+        else
+        {
+            OnDie?.Invoke();
+            Die();
+        }
+    }
+
+    public void Die()
+    {
+        //Destroy(gameObject, 3f);
+        gameObject.SetActive(false);
+    }
+    
+    private void OnDestroy()
+    {
+        Dispose();
+    }
+
+    public void Dispose()
+    {
+        OnTakeDamage -= OnTakeDamage;
+        OnDie -= OnDie;
+    }
+}
