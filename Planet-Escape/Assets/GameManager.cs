3b051d706ad5b8c50838caa19c0ffe0afd3e5b29
Julian Lanty <julianemanueltejo@gmail.com>
2023-11-27 02:28:23 -0300

    Instantiate Encounters
    Load Map on new game from main menu

--- Planet-Escape/Assets/GameManager.cs
+++ Planet-Escape/Assets/GameManager.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Map;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Singleton;
-    public List<Card_SO> PlayersDeck;
+    public List<Card_SO> PlayersDeck=new List<Card_SO>();
+    public int playerHealth;
+    public NodeType CurrentNodeType = NodeType.MinorEnemy;
     private void Awake()
     {
         if (Singleton == null)
